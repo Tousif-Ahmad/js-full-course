a42fb2b31117b1893018cef39bfbdbceb457c77a
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-07-30 03:48:34 +0500

    data types

---
+++ 02-datatypes/02-datatypes.js
@@ -0,0 +1,24 @@
+"use strict";   /* treat all js code as newer version */
+// alert("Hello");  /* we are using node js not browser  */
+
+let name = "Sam";
+let age = 22;
+let isLoggedIn = false;
+let state;
+let temp = null;
+
+//  primitive data types 
+// number => 2 to power 53 
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+//  symbol => store unique value 
+
+// console.log(typeof name);
+// console.log(typeof null);
+// console.log(typeof undefined);
+
+// Objects
+
