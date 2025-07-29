f4edae8f0491bcbfface1afab12a40381f6767c6
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-07-30 04:21:36 +0500

    string conversion and math operations

---
+++ 01-basics/03-conversion-operations.js
@@ -0,0 +1,55 @@
+// let score = "sam";
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNum = Number(score);
+// console.log(typeof valueInNum);
+// console.log(valueInNum);
+
+// "33" => 33 
+// "33abc" => NAN (Not a Number)
+//  true => 1
+// null = 0
+
+
+// let isLoggedIn = false;
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+// let someNum = 33;
+// let strNum = String(someNum)
+// console.log(someNum);
+// console.log(typeof strNum);
+
+
+// ========================= OPERATIONS ============================================//
+// let value = 3
+// let negValue = -value;
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 ** 3);
+// console.log(2 % 3);
+
+
+// let str1 = "hello";
+// let str2 = " Sam";
+
+// let str = str1 + str2;
+// console.log(str);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+ "");
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
