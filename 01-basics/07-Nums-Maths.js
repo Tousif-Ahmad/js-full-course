9e19aca464ff042e8ed942a762a28039eb6c8a00
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-07-31 02:42:05 +0500

    Nums and Maths

---
+++ 01-basics/07-Nums-Maths.js
@@ -0,0 +1,31 @@
+// const score = 500;
+// console.log(score);
+
+// const balance = new Number(400);
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+// const otherNum = 23.2467;
+// console.log(otherNum.toPrecision(3));
+
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-PK'));
+
+
+// ======================== MATHS =====================================//
+// console.log(Math.abs(-3));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(2.3));
+// console.log(Math.floor(5.6));
+// console.log(Math.min(2,34));
+// console.log(Math.max(3,4));
+
+// console.log(Math.random());
+// console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
