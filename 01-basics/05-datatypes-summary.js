0aaa9bbe851042c47ee3e475e50492a158e76071
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-07-30 05:06:15 +0500

    Data types Summary

---
+++ 01-basics/05-datatypes-summary.js
@@ -0,0 +1,34 @@
+// primitive data types 
+
+// 7 types : Strings, Number, Boolean, null, undefined, Bigint, Symbol
+
+let score = 23;
+const scoreVal = 23.45;
+
+const isLoggedIn = false;
+const temp = null;
+let userEmail;
+
+// const id = Symbol("123");
+// const anotherId = Symbol("123");
+// console.log(id === anotherId);
+
+// const bigNum = 87438957394857398457934875348957348957348957948n;
+
+
+// Reference (Non Primitive types)
+// Array, Objects, Functions 
+
+// Arrays
+const heroes = ["BatMan", "spiderMan",];
+// Object
+const user = {
+    name : "ali",
+    age : 22,
+}
+
+// Functions
+let sayHi = function(){
+    console.log("Hello");
+}
+sayHi()
