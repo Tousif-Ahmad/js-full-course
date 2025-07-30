ce3ac44068743b982663521f48274b444f8912d3
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-07-31 02:15:31 +0500

    strings

---
+++ 01-basics/06-strings.js
@@ -0,0 +1,13 @@
+const name = "Sam";
+const repoCount = 20;
+// console.log(name + repoCount);
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}.`);
+
+const gameCount = new String("Sam");
+// console.log(gameCount[0]);
+// console.log(gameCount.__proto);
+
+// console.log(gameCount.charAt(2));
+// console.log(gameCount.indexOf("S"));
+// console.log(gameCount.toUpperCase());
+
