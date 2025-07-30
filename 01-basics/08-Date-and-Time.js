89c2d4fd434abba5e609b58cf7d3f8d60954bebc
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-07-31 03:10:22 +0500

    Date and Time

---
+++ 01-basics/08-Date-and-Time.js
@@ -0,0 +1,22 @@
+// Dates 
+// let date = new Date();
+// console.log(date);
+// console.log(date.toString());
+// console.log(date.toISOString());
+// console.log(date.toJSON());
+// console.log(date.toLocaleDateString());
+// console.log(date.toLocaleString());
+// console.log(date.toLocaleTimeString());
+// console.log(date.toTimeString());
+// console.log(date.toUTCString());
+
+// let myCreatedDate = new Date(2025, 0, 23);
+// let myCreatedDate = new Date(2025, 0, 23, 4, 4);
+let myCreatedDate = new Date("01-14-2025");
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamps = Date.now();
+// console.log(myTimeStamps);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now() / 1000));
+
