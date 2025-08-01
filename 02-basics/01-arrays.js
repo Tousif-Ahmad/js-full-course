a1fd4987e166e9c57e441d87dc79429542e85df7
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-08-01 21:18:05 +0500

    Arrays

---
+++ 02-basics/01-arrays.js
@@ -0,0 +1,56 @@
+// ARRAYS 
+const myArray = [1,2,3,4,5];
+const heroes = ["spiderman", "Batman"];
+
+// const myArr2 = new Array(1,2,3,4);
+// console.log(myArr2[0]);
+
+// ARRAY METHODS 
+// myArray.push(6);
+// myArray.pop();
+// myArray.unshift(0);
+// myArray.shift()
+
+// console.log(myArray.includes(1));
+// console.log(myArray.indexOf(2));
+
+// const newArr = myArray.join();
+// console.log(myArray);
+// console.log(newArr);
+
+
+// slice splice 
+// console.log("A ", myArray);
+
+// const myn1 = myArray.slice(1,3);
+// console.log(myn1);
+// console.log("B", myArray);
+
+// const myn2 = myArray.splice(1,3);
+// console.log("C", myArray);
+// console.log(myn2);
+
+
+const marvelHeroes = ["Thor", "ironman", "Spiderman"];
+const DcHeroes = ["Superman", "flash", "batman"];
+
+// let allHeroes = marvelHeroes.concat(DcHeroes);
+// console.log(allHeroes);
+
+// const allHeroes = [...marvelHeroes, ...DcHeroes];
+// console.log(allHeroes);
+
+
+// const anotherArr = [1,2,3, [4,5,6], 7, [6,7, [4,5]]];
+// const realAnotherArr = anotherArr.flat(Infinity);
+// console.log(realAnotherArr);
+
+
+// console.log(Array.isArray("Ali"));
+// console.log(Array.from("ali"));
+// console.log(Array.from({name : "ali"}));  /* interview questions it gives empty  array*/
+
+// let score1 = 100;
+// let score2 = 200;
+// let score3 = 300;
+// console.log(Array.of(score1, score2, score3));
