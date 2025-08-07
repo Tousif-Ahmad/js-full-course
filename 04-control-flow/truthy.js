1d2d3189b7cf44a6110ce164995520e2cd1d9064
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-08-07 18:40:11 +0500

    control flow

---
+++ 04-control-flow/truthy.js
@@ -0,0 +1,50 @@
+const userEmail = "S@gmail.com";
+
+// if (userEmail){
+//     console.log("Got user email");
+// }
+// else{
+//     console.log("Don't have user email");
+// }
+
+
+// falsy values 
+// 1. false 
+// 2. 0
+// 3. -0
+// 4. 0n
+// 5. ""
+// 6. null
+// 7. undefined 
+// 8. NAN
+
+// ALL OTHERS ARE TRUTHY VALUES.
+
+// const empObj = {};
+// if (Object.keys(empObj).length === 0){
+//     console.log("Object is empty");
+// }
+
+
+
+// NULLISH COALESCING OPERATOR (??) : NULL, UNDEFINED 
+// let val1;
+// val1 = 5 ?? 10;
+// val1 = null ?? 10
+// val1 = undefined ?? 15;
+// val1 = null ?? 10 ?? 20;
+// console.log(val1);
+
+// e.g 2
+// console.log(0 ?? null);
+// console.log(false ?? undefined);
+// console.log("" ?? null);
+
+// TERNARY OPERATOR 
+// syntax 
+// condition ? true : false 
+
+// let age = 16;
+// let res = age >= 18 ? console.log("You can vote") : console.log("You can't vote");
+
+
