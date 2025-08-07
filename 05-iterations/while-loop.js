d7c07c760e4776797db8d7058505a9595c958d87
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-08-08 00:17:33 +0500

    while and Do while loop

---
+++ 05-iterations/while-loop.js
@@ -0,0 +1,22 @@
+//  while loop
+// let i = 0
+// while (i <= 10) {
+    // console.log(`value of i is ${i}`);
+    // i+=2;
+// }
+
+// const myArray = ["flash", "Batman", "spiderman"];
+// let i = 0;
+// while (i < myArray.length) {
+//     console.log(myArray[i]);
+//     i++;
+// }
+
+
+// DO WHILE LOOP
+// let score = 17;
+// do{
+//     console.log(score);
+//     score++;
+// }
+// while(score <= 10)
